6ceb9be174b04f49b3fe72689cbd62abca600d40
190103440 <190103440@stu.sdu.edu.kz>
2021-02-08 04:55:33 +0600

    8

---
+++ lab.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Batyrkhan Daniyar</title>
+</head>
+<style>
+  body { background: url(https://ktonanovenkogo.ru/image/mars-krasnaia-planeta.jpg); }
+</style>
+<br> <br>
+<table style="width:70%" border="5" bordercolor="#0000ff" bgcolor="yellow"; >
+<tr>
+    <td colspan="2"> <center> <font color="red" size="16" > Technology of pharmaceutical production </font> 
+    </td>
+</tr>
+
+<tr>
+    
+    <td width="40%"> <center> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQZrizvT-yxMNHaPmztxbWHI0DcCNyZLbgaY5lGy1xRulhSwD-5&usqp=CAU" style="width:80%;height:85%" alt="production pharmacy">   
+     </td>
+     
+     <td width="50%"> 
+     <ol>
+      <li> Group TFP20-015 </li>
+      <li>Group TFP20-016</li>
+      <li>Group TFP20-017</li>
+      <li>Group TFP20-018</li>
+      </ol>
+     
+     </td>
+        
+       
+    </td>
+ </tr>
+ 
+ <tr>
+    <td colspan="2"> <center> <font color="green" size="16" > Information and communication technologies </font> 
+    
+     </td>
+        
+               
+    </td>
+ </tr>
+  
+</table>
+<br>
+<br>
+
+
+<hr size="5" color="white">
+
+<p> <font color="white" size="5"> <b> N(Me(OH<sub>2</sub>))=3,01*10<sup>23</sup> </b></font>
+
+</body>
+</html>
